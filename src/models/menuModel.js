2440fb47f535a94ec76f2334a059aca9bb4b47fa
Muhammad Nova Stella <122338377+novastellaa@users.noreply.github.com>
2023-10-20 14:23:06 +0700

    Update menuModel.js

--- src/models/menuModel.js
+++ src/models/menuModel.js
@@ -1,24 +1,83 @@
 const db = require("../../db/config")
 
 const menuModel = {}
+
+
 menuModel.getAll = () => {
-    return db.all("SELECT * FROM menu",(err,rows) => {
-        if(err) {
-            throw err
-        }else {
-            return rows
-        }
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM menu", (err, rows) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
     })
+
 }
-// lanjutkan disini
+
 menuModel.create = (data) => {
-    return db.run(`INSERT INTO menu (name,price) VALUES ('${data.name,data.price}')`,(err,rows) => {
-        if(err) {
-            throw err
-        }else {
-            return rows
-        }
+    return new Promise((resolve, reject) => {
+        db.run(`INSERT INTO menu (item,price) VALUES ('${data.item}', '${data.price}')`, (err, rows) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
     })
 }
+
+menuModel.getById = (id) => {
+    return new Promise((resolve, reject) => {
+        db.get("SELECT * FROM menu WHERE id = ?", [id], (err, rows) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
+    })
+}
+
+menuModel.update = (id, item, price) => {
+    return new Promise((resolve, reject) => {
+        db.run("UPDATE menu SET item = ?, price = ? WHERE id = ?", [item, price, id], (err, rows) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
+
+    })
+}
+
+menuModel.delete = (id) => {
+    return new Promise((resolve, reject) => [
+        db.run("DELETE FROM menu WHERE id = ?", [id], (err, rows) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(rows)
+            }
+        })
+    ])
+}
+
+menuModel.select = (menuNames, callback) => {
+    const placeholders = menuNames.map(() => '?').join(',');
+    const query = `SELECT * FROM menu WHERE item IN (${placeholders})`;
+
+    db.all(query, menuNames, (err, rows) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, rows);
+        }
+    });
+};
+
+
 
 module.exports = menuModel
