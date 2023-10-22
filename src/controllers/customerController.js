0d026de5bd0f9a0937cb8661c720775083569191
Muhammad Nova Stella <122338377+novastellaa@users.noreply.github.com>
2023-10-22 19:22:34 +0700

    Update customerController.js

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,43 +1,94 @@
 const customerModel = require("../models/customerModel");
 
+
+// functiom validasi
 function isAlpha(str) {
     return /^[A-Za-z\s]+$/.test(str);
 }
 
+const customerController = {}
 
-const customerControllers = {};
+customerController.getAll = (req, res) => {
+    customerModel.getAll((err, customer) => {
+        if (err) {
+            res.status(500).json({ error: 'Terjadi kesalahan dalam mengambil data menu.' });
+        } else {
+            res.json({ customer });
+        }
+    });
+};
 
-customerControllers.getAll = async(req, res) => {
-    const data = await customerModel.getAll();
-    res.json({
-        status: "OK",
-        data
-    })
-}
-
-
-customerControllers.create = async(req, res) => {
+// Metode create untuk customer
+customerController.create = async(req, res) => {
     const { name, address, email } = req.body;
-
-    // Validasi kolom item
-    if (!item || !isAlpha(name)) {
+    if (!name || !isAlpha(name)) {
         return res.status(400).json({ error: 'Kolom name harus diisi dengan huruf.' });
     }
-    if (!item || !isAlpha(address)) {
+
+    // Validasi kolom price
+    if (!address || !isAlpha(address)) {
         return res.status(400).json({ error: 'Kolom address harus diisi dengan huruf.' });
     }
-    if (!item || !isAlpha(email)) {
+
+    if (!email || !isAlpha(email)) {
         return res.status(400).json({ error: 'Kolom email harus diisi dengan huruf.' });
     }
+
     // Lanjutkan dengan menyimpan data menu jika validasi sukses
-    await menuModel.create(req.body);
+    await customerModel.create(req.body);
     res.json({
         status: "OK",
         message: "Data berhasil ditambahkan"
     });
 };
 
-customerControllers.getById = async(req, res) => {
+// Metode update untuk customer
+customerController.update = (req, res) => {
+    const customerId = req.params.id;
+    const updatedCustomer = req.body;
+
+    customerModel.update(customerId, updatedCustomer, (err, updatedData) => {
+        if (err) {
+            res.status(500).json({ error: 'Terjadi kesalahan dalam mengupdate data pelanggan.' });
+        } else {
+            res.json({
+                status: "OKE",
+                message: "data berhasil di perbarui"
+            });
+        }
+    });
+};
+
+// Metode delete untuk customer
+customerController.delete = (req, res) => {
+    const customerId = req.params.id;
+
+    customerModel.delete(customerId);
+    res.json({
+        status: "OKE",
+        message: "data berhasil di hapus"
+    });
+};
+
+
+// 
+customerController.getByName = (req, res) => {
+    const customerId = req.params.id; // Mengambil customerId dari parameter permintaan
+
+    customerModel.getByName(customerId, (err, customer) => {
+        if (err) {
+            return res.status(500).json({ error: 'Terjadi kesalahan dalam mengambil data pelanggan.' });
+        }
+
+        if (!customer) {
+            return res.status(404).json({ message: 'Pelanggan tidak ditemukan.' });
+        }
+
+        res.json({ customer });
+    });
+};
+
+customerController.getById = async(req, res) => {
     const { id } = req.params;
     const data = await customerModel.getById(id);
     res.json({
@@ -46,39 +97,4 @@ customerControllers.getById = async(req, res) => {
     })
 }
 
-customerControllers.update = async(req, res) => {
-    const { id } = req.params;
-    const { name, address, email } = req.body;
-
-    await customerModel.updateById(name, address, email, id);
-
-    res.json({
-        status: "OK",
-        message: "data berhasil diperbarui"
-    })
-}
-
-
-customerControllers.delete = async(req, res) => {
-    const { id } = req.params;
-
-    await customerModel.deleteById(id);
-
-    res.json({
-        status: "OK",
-        message: "data berhasil dihapus"
-    })
-}
-
-customerControllers.clearAllData = async(req, res) => {
-    await customerModel.clearAllDataTable()
-    res.json({
-        status: "OK",
-        message: "semua data berhasil di hapus"
-    })
-}
-
-
-
-
-module.exports = customerControllers
+module.exports = customerController;
