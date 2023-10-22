c087ae94306949ab69fee562596f67e4cfeffc20
Muhammad Nova Stella <122338377+novastellaa@users.noreply.github.com>
2023-10-22 19:27:10 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,31 @@
-const express = require("express")
-const menuController = require("../controllers/menuController")
-const customerController = require("../controllers/customerController")
-const categoriesController = require("../controllers/categoriesControllers")
-const orderControllers = require("../controllers/orderControllers")
-const router = express.Router()
+const express = require("express");
+const menuController = require("../controllers/menuController"); // Pastikan controller sudah diimpor dengan benar
+const customerController = require('../controllers/customerController');
+const categoriesController = require('../controllers/categoriesController');
+const orderController = require('../controllers/orderController');
+const historyController = require('../controllers/historyController');
+const router = express.Router();
+
+//router menu
+router.get('/menus', menuController.getAll); // Mendapatkan semua menu
+router.post('/menus', menuController.create); // Menambahkan menu baru
+router.put('/menus/:id', menuController.update); // Mengupdate menu berdasarkan ID
+router.delete('/menus/:id', menuController.delete); // Menghapus menu berdasarkan ID
 
 
-//Route untuk table menus
-router.get('/menus', menuController.getAll)
-router.post('/menus', menuController.create)
-router.get('/menus/:id', menuController.getById)
-router.put('/menus/:id', menuController.updateData)
-router.delete('/menus/:id', menuController.deleteData)
+// router customer
+router.get('/customers', customerController.getAll); // Mendapatkan semua pelanggan
+router.post('/customers', customerController.create); // Menambahkan pelanggan baru
+router.put('/customers/:id', customerController.update); // Mengupdate pelanggan berdasarkan ID
+router.delete('/customers/:id', customerController.delete); // Menghapus pelanggan berdasarkan ID
+router.get('/customers/:id', customerController.getByName);
 
+// router categories
+router.get('/categories', categoriesController.getAll); // Mendapatkan semua kategori
+router.post('/categories', categoriesController.create); // Menambahkan kategori baru
+router.put('/categories/:id', categoriesController.update); // Mengupdate kategori berdasarkan ID
+router.delete('/categories/:id', categoriesController.delete); // Menghapus kategori berdasarkan ID
 
-
-//routes untuk table customers
-router.get("/customers", customerController.getAll);
-router.post("/customers", customerController.create);
-router.get("/customers/:id", customerController.getById);
-router.put("/customers/:id", customerController.update);
-router.delete("/customers/:id", customerController.delete);
-router.delete("/customers/", customerController.clearAllData);
-
-
-//routes untuk table categories
-router.get("/categories", categoriesController.getAll)
-router.post("/categories", categoriesController.create)
-router.get("/categories/:id", categoriesController.getById)
-router.put("/categories/:id", categoriesController.updateCategories)
-router.delete("/categories/:id", categoriesController.deleteById)
-
-
-//routes untuk table orders
-router.post("/orders", orderControllers.create)
-router.get("/orders", orderControllers.getAll)
-
-module.exports = router
+// router order
+router.post('/orders', orderController.create);
+router.get('/orders', orderController.getAll);
